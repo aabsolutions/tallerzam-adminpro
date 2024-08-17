6c69f826136bfaf0aa79aec3d4ee9cc3edb46f5a
aabsolutions <88517644+aabsolutions@users.noreply.github.com>
2024-08-17 14:40:45 -0500

    Focus to internet backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    //base_url : 'https://taller-backend.onrender.com/api',
-    base_url : 'http://localhost:3000/api'
+    base_url : 'https://taller-backend-5ruc.onrender.com/api'
+    //base_url : 'http://localhost:3000/api'
 };
